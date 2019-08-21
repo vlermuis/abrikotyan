89c5a839a34d67d6e682d4e2d9a938b514c3886f
Alex Li <Alex.Li@tymphany.com>
2019-08-21 15:18:06 +0800

    Add a diag for connection
    Signed-off-by: Alex Li <lizhiqin46783937@live.com>

--- Android/Abrikotyan/app/src/main/java/com/tymphany/abrikotyan/MainActivity.java
+++ Android/Abrikotyan/app/src/main/java/com/tymphany/abrikotyan/MainActivity.java
@@ -1,7 +1,15 @@
 package com.tymphany.abrikotyan;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
@@ -9,16 +17,15 @@ import static java.lang.Math.PI;
 
 public class MainActivity extends AppCompatActivity {
 
-    final String serverIP = "10.10.10.4";
-    final int serverPort = 3490;
+    final String defaultServerIP = "10.10.10.4";
+    final int defaultServerPort = 3490;
+    final SocketClient socketClient = new SocketClient(MainActivity.this);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final SocketClient socketClient = new SocketClient(MainActivity.this);
-        socketClient.connect(serverIP,serverPort);
 
         JoystickView joystickViewTurn = findViewById(R.id.joyStickLeft);
 
@@ -27,6 +34,26 @@ public class MainActivity extends AppCompatActivity {
             public void onMove(int angle, int strength) {
                 MotorState newMotorState = calcMotorState(angle, strength);
                 socketClient.send(newMotorState.toBytes());
+            }
+        });
+
+        Button connectButton = findViewById(R.id.connectButton);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final EditText et = new EditText(MainActivity.this);
+                et.setText(defaultServerIP + ":" + defaultServerPort);
+                new AlertDialog.Builder(MainActivity.this).setTitle("Input IP address:port")
+                        .setIcon(R.drawable.ic_launcher_foreground)
+                        .setView(et)
+                        .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Pattern pen = Pattern.compile(":");
+                                String[] subString = pen.split(et.getText());
+                                socketClient.connect(subString[0], Integer.parseInt(subString[1]));
+                            }
+                        }).setNegativeButton("Cancel", null).show();
             }
         });
 
